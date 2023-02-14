c992419f3d215db9953616669b783cf180d54387
Khaled Elawady <abo.amar361@yahoo.com>
2023-02-14 23:31:32 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const header = document.querySelector('.landing .heading-text');
+
+const aboutSection = document.querySelector('.about ');
+window.onscroll = function () {
+    const aboutOffsetTop = aboutSection.offsetTop;
+    
+    const aboutOuterHeight = aboutSection.offsetHeight;
+    
+    const headerOffsetTop = header.offsetTop;
+    
+    const headerOuterHeight = header.offsetHeight;
+    const windowHeight = this.innerHeight;
+    
+    const windowScrollTop = this.pageYOffset;
+
+    const headerNav = document.querySelector('.landing header'); 
+    if (windowScrollTop > (headerOffsetTop + headerOuterHeight - windowHeight)) {
+        headerNav.classList.add('active');
+    }
+    if (windowScrollTop > (aboutOffsetTop + aboutOuterHeight - windowHeight)) {
+        aboutSection.classList.add('opacity');
+        const contactSection = document.querySelector('.contact');
+        contactSection.classList.add('opacity-two');
+    };
+}
