629593324d93f98743c56517a101c77a2504e81c
Khaled Elawady <abo.amar361@yahoo.com>
2023-05-13 05:41:28 +0300

    Update about-product.js

--- Tech Online Store/js/about-product.js
+++ Tech Online Store/js/about-product.js
@@ -15,4 +15,27 @@ function removeAll() {
   productInfo.forEach((item) => {
     item.classList.remove("active");
   });
+  aboutProInfo.classList.remove("active");
+  detailProInfo.classList.remove("active");
+  specsProInfo.classList.remove("active");
 }
+
+const aboutProducter = document.querySelector(".about-product .about-pro");
+const aboutProInfo = document.querySelector(".product-content .about-pro");
+
+aboutProducter.onclick = () => {
+  aboutProInfo.classList.add("active");
+};
+
+const detailProducter = document.querySelector(".about-product .detail");
+const detailProInfo = document.querySelector(".product-content .details");
+
+detailProducter.onclick = () => {
+  detailProInfo.classList.add("active");
+};
+const specsProducter = document.querySelector(".about-product .specs");
+const specsProInfo = document.querySelector(".product-content .space");
+
+specsProducter.onclick = () => {
+  specsProInfo.classList.add("active");
+};
