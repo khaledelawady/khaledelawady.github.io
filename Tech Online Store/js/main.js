2755502b93377cd009d09807674652ba96f8d26e
Khaled Elawady <abo.amar361@yahoo.com>
2023-05-19 02:13:17 +0300

    Update main.js

--- Tech Online Store/js/main.js
+++ Tech Online Store/js/main.js
@@ -140,3 +140,73 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for (let i = 0; i < marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+
+// add cart
+const addCartMain = document.querySelectorAll(".main_ .add-cart");
+
+addCartMain.forEach((cartlink) => {
+  cartlink.addEventListener("click", function () {
+    // vibe cart icon
+    document.querySelector(".fa-cart-shopping").classList.add("active");
+    // Get Cart Img When Click Adding
+    const imgCart = this.parentElement.children[2].cloneNode(true);
+    // Get Cart text When Click Adding
+    const textCart = this.parentElement.children[4].cloneNode(true);
+    const cartContent = document.createElement("div");
+    const countProducter = document.createElement("span");
+    const Icons = document.createElement("div");
+    const cancelIcon = document.createElement("i");
+    const cancelLink = document.createElement("a");
+    const editIcon = document.createElement("i");
+    const editLink = document.createElement("a");
+    cartContent.className = "cart-content";
+    cartContainer.style.cssText =
+      "display: flex;flex-direction: column;gap: 10px;";
+    cartContent.style.cssText =
+      "display: flex;align-items: center;gap: 10px; width: fit-content;border-top: 2px solid var(--text-color);padding: 20px 10px;";
+    countProducter.textContent = "x1";
+    imgCart.style.cssText = "width: 50px;";
+    textCart.style.cssText = "font-size: 14px;font-weight: 500;";
+    Icons.style.cssText = "display: flex;flex-direction: column;gap: 10px;";
+    cancelIcon.className = "fa-solid fa-xmark";
+    cancelLink.href = "##";
+    cancelLink.onclick = removeCountPro;
+    cancelLink.style.cssText =
+      "padding: 4px; border: 2px solid var(--text-color); border-radius: 50%;color: var(--text-color);font-size: 12px;cursor: pointer;";
+    editIcon.className = "fa-solid fa-pencil";
+    editLink.href = "##";
+    editLink.style.cssText =
+      "padding: 4px; border: 2px solid var(--text-color); border-radius: 50%;color: var(--text-color);font-size: 12px;cursor: pointer;";
+    cartContent.appendChild(countProducter);
+    cartContent.appendChild(imgCart);
+    cartContent.appendChild(textCart);
+    cartContent.appendChild(textCart);
+    cancelLink.appendChild(cancelIcon);
+    editLink.appendChild(editIcon);
+    Icons.appendChild(cancelLink);
+    Icons.appendChild(editLink);
+    cartContent.appendChild(Icons);
+    cartContainer.appendChild(cartContent);
+    // big cart
+    const cartProductContainer = document.querySelector(
+      ".cart-products-container"
+    );
+    const bigCart = cartContent.cloneNode(true);
+    bigCart.childNodes[1].style.width = "100px";
+    bigCart.childNodes[0].style.fontSize = "16px";
+    bigCart.childNodes[3].childNodes[0].style.padding = "4px 6px";
+    bigCart.childNodes[3].childNodes[0].onclick = removeCountPro;
+    cartProductContainer.appendChild(bigCart);
+    // remove
+    function removeCountPro() {
+      bigCart.remove();
+      cartContent.remove();
+      countElementMin();
+    }
+    // less than 3 div in hover cart
+    // const allCartContent = document.querySelectorAll(".cart-content");
+    // if (allCartContent.length > 2) {
+    //   allCartContent[3].remove();
+    // }
+  });
+});
