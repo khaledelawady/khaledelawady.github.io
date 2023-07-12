f4e3d1c625141835558e1f6bfc51b1c67b0ba438
Khaled Elawady <abo.amar361@yahoo.com>
2023-07-12 05:51:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-const header = document.querySelector('.landing .heading-text');
+const header = document.querySelector(".landing .heading-text");
 
-const aboutSection = document.querySelector('.about ');
+const aboutSection = document.querySelector(".about ");
 window.onscroll = function () {
-    const aboutOffsetTop = aboutSection.offsetTop;
-    
-    const aboutOuterHeight = aboutSection.offsetHeight;
-    
-    const headerOffsetTop = header.offsetTop;
-    
-    const headerOuterHeight = header.offsetHeight;
-    
-    const windowHeight = this.innerHeight;
-    
-    const windowScrollTop = this.pageYOffset;
+  const aboutOffsetTop = aboutSection.offsetTop;
 
-    const headerNav = document.querySelector('.landing header'); 
-    if (windowScrollTop > (headerOffsetTop + headerOuterHeight - windowHeight)) {
-        headerNav.classList.add('active');
-    }
-    if (windowScrollTop > (aboutOffsetTop + aboutOuterHeight - windowHeight)) {
-        aboutSection.classList.add('opacity');
-        const contactSection = document.querySelector('.contact');
-        contactSection.classList.add('opacity-two');
-    };
-}
+  const aboutOuterHeight = aboutSection.offsetHeight;
+
+  const windowHeight = this.innerHeight;
+
+  const windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop > aboutOffsetTop + aboutOuterHeight - windowHeight) {
+    aboutSection.classList.add("opacity");
+    const contactSection = document.querySelector(".contact");
+    contactSection.classList.add("opacity-two");
+  }
+};
